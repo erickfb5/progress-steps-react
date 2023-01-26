8639ab2adfba9e03405f7f3dc18c93aacbf5e2d7
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-26 01:54:43 -0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [currentActive, setCurrentActive] = useState(1);
+  const steps = [1, 2, 3, 4];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="progress-container">
+        <div
+          className="progress"
+          style={{ width: `${((currentActive - 1) / 3) * 100}%` }}
+        ></div>
+        {steps.map((num) => (
+          <div
+            className={`circle ${num <= currentActive ? "active" : ""}`}
+            key={num}
+            onClick={() => setCurrentActive(num)}
+          >
+            {num}
+          </div>
+        ))}
+      </div>
+      <button
+        className="btn"
+        id="prev"
+        disabled={currentActive === steps[0]}
+        onClick={() => setCurrentActive(currentActive - 1)}
+      >
+        Prev
+      </button>
+      <button
+        className="btn"
+        id="next"
+        disabled={currentActive === steps[steps.length - 1]}
+        onClick={() => setCurrentActive(currentActive + 1)}
+      >
+        Next
+      </button>
     </div>
   );
-}
+};
 
 export default App;
